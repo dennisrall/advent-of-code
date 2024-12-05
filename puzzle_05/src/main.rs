a05b020ad9948fa3d2fe0b1f1d6a98da25f986d3
Dennis Rall <dennis.rall@smart-cybersecurity.de>
2024-12-05 11:12:43 +0100

    feat: puzzle 05

---
+++ puzzle_05/src/main.rs
@@ -0,0 +1,73 @@
+use std::{
+    cmp::Ordering,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use ordering::{OrderItem, OrderingRule};
+
+mod ordering;
+
+fn main() {
+    let rules: Vec<_> = BufReader::new(File::open("rules.txt").unwrap())
+        .lines()
+        .filter_map(|l| l.ok())
+        .filter_map(|l| OrderingRule::from_str(&l))
+        .collect();
+
+    let sum_middle_items_correct: OrderItem = BufReader::new(File::open("sequences.txt").unwrap())
+        .lines()
+        .filter_map(|s| s.ok())
+        .map(|s| {
+            s.split(",")
+                .filter_map(|v| v.parse::<OrderItem>().ok())
+                .collect::<Vec<_>>()
+        })
+        .filter(|s| rules.iter().all(|r| r.is_fullfilled(s)))
+        .map(|s| *s.get(s.len() / 2).unwrap())
+        .sum();
+    println!("sum: {}", sum_middle_items_correct);
+
+    let sum_middle_items_reordered: OrderItem =
+        BufReader::new(File::open("sequences.txt").unwrap())
+            .lines()
+            .filter_map(|s| s.ok())
+            .map(|s| create_sequence(s))
+            .filter(|s| rules.iter().any(|r| !r.is_fullfilled(s)))
+            .map(|s| order_correctly(&s, &rules))
+            .map(|s| *s.get(s.len() / 2).unwrap())
+            .sum();
+
+    println!("sum: {}", sum_middle_items_reordered);
+}
+
+fn create_sequence(s: String) -> Vec<OrderItem> {
+    s.split(",")
+        .filter_map(|v| v.parse::<OrderItem>().ok())
+        .collect::<Vec<_>>()
+}
+
+fn order_correctly(seq: &Vec<OrderItem>, rules: &[OrderingRule]) -> Vec<OrderItem> {
+    let compare_by_rules = |a: &OrderItem, b: &OrderItem| {
+        if rules
+            .iter()
+            .find(|r| r.before == *a && r.after == *b)
+            .is_some()
+        {
+            Ordering::Less
+        } else if rules
+            .iter()
+            .find(|r| r.before == *b && r.after == *a)
+            .is_some()
+        {
+            Ordering::Greater
+        } else {
+            Ordering::Equal
+        }
+    };
+
+    let mut s = seq.clone();
+
+    s.sort_by(compare_by_rules);
+    s
+}
---
+++ puzzle_05/src/ordering.rs
@@ -0,0 +1,101 @@
+pub type OrderItem = usize;
+
+#[derive(Debug)]
+pub struct OrderingRule {
+    pub before: OrderItem,
+    pub after: OrderItem,
+}
+
+impl OrderingRule {
+    pub fn from_str(s: &str) -> Option<OrderingRule> {
+        let mut parts = s.split("|");
+
+        let before = parts.next()?.parse().ok()?;
+        let after = parts.next()?.parse().ok()?;
+
+        Some(OrderingRule { before, after })
+    }
+
+    pub fn is_fullfilled(&self, sequence: &Vec<OrderItem>) -> bool {
+        match (
+            sequence.iter().position(|&item| item == self.before),
+            sequence.iter().position(|&item| item == self.after),
+        ) {
+            (Some(before), Some(after)) => before < after,
+            _ => true,
+        }
+    }
+
+    pub fn make_fullfilled_if_not(&self, sequence: &mut Vec<OrderItem>) {
+        if self.is_fullfilled(sequence) {
+            return;
+        }
+        let before = sequence
+            .iter()
+            .position(|&item| item == self.before)
+            .unwrap();
+        let after = sequence
+            .iter()
+            .position(|&item| item == self.after)
+            .unwrap();
+
+        sequence.swap(after, before);
+    }
+}
+
+pub fn check_ordering() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_str() {
+        let result = OrderingRule::from_str("425|27").unwrap();
+
+        assert_eq!(result.before, 425);
+        assert_eq!(result.after, 27);
+    }
+
+    #[test]
+    fn test_is_fullfilled_empty() {
+        let rule = OrderingRule::from_str("42|27").unwrap();
+        let result = rule.is_fullfilled(&vec![]);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_is_fullfilled_both_not_present() {
+        let rule = OrderingRule::from_str("42|27").unwrap();
+        let result = rule.is_fullfilled(&vec![1, 2, 3, 4]);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_is_fullfilled_before_not_present() {
+        let rule = OrderingRule::from_str("42|27").unwrap();
+        let result = rule.is_fullfilled(&vec![100, 27, 13]);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_is_fullfilled_after_not_present() {
+        let rule = OrderingRule::from_str("42|27").unwrap();
+        let result = rule.is_fullfilled(&vec![10, 42, 13]);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_is_fullfilled_correct() {
+        let rule = OrderingRule::from_str("42|27").unwrap();
+        let result = rule.is_fullfilled(&vec![10, 42, 13, 27]);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_is_fullfilled_false() {
+        let rule = OrderingRule::from_str("42|27").unwrap();
+        let result = rule.is_fullfilled(&vec![27, 10, 42, 13]);
+        assert_eq!(result, false);
+    }
+}
